eff503262fec97e56022fe222e39baa661f6d7a0
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-11-14 18:08:50 +0000

    teste 01

--- tp_02/calculadora_oo.py
+++ tp_02/calculadora_oo.py
@@ -1,16 +1,16 @@
 class calculadora():
     def soma(operando_a, operando_b):
-        operando_a + operando_b
+       return operando_a + operando_b
     
     def subtrai(operando_a, operando_b):
-        operando_a - operando_b
+        return operando_a - operando_b
     
     def multiplica(operando_a, operando_b):
-        operando_a * operando_b
+        return operando_a * operando_b
 
     def dividi(operando_a, operando_b):
         try:
-            operando_a/operando_b
+            return operando_a/operando_b
         except:
             print('Divisão por zero não existe.')
 
