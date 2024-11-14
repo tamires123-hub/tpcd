b25a129ae85077d0cc822d9764853d04de4590ba
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-11-14 18:08:24 +0000

    4 operações

---
+++ tp_02/testa_calculadora.py
@@ -0,0 +1,7 @@
+from calculadora_oo import calculadora as calc
+
+def teste():
+    result_soma = print(calc.soma(20, 24))
+    result_subtrai = print(calc.subtrai(20, 24))
+    result_multiplica = print(calc.multiplica(20, 24))
+    result_dividi = print(calc.dividi(20, 24))
