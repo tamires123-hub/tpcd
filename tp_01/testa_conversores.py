9375964db3e02ea2e10c1581d0772c1b24aacada
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-11-21 18:06:59 +0000

    testando o módulo de conversão

---
+++ tp_01/testa_conversores.py
@@ -0,0 +1,12 @@
+import conversores as c
+
+def teste():
+    celsius_fahrenheit = print(c.celsius_para_fahrenheit(30))
+    fahrenheit_celsius = print(c.fahrenheit_para_celsius(30))
+    metrp_pes = print(c.metro_para_pes(30))
+    pes_metro = print(c.pes_para_metro(30))
+    km_milhas = print(c.quilometro_para_milhas(30))
+    milhas_km = print(c.milhas_para_quilometro(30))
+    dia_hora = print(c.dia_para_horas(30))
+
+teste()
