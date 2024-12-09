b1d32aacd62fc9e0d68fb894503e404b4cd24a57
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-12-09 17:11:44 +0000

    atualizando o elif 7

--- tp_02/terminal_atendimento.py
+++ tp_02/terminal_atendimento.py
@@ -18,37 +18,41 @@ def terminal():
             conta = Conta(numero)
             conta.cadastrar()
 
-        if opcao == 1:
+        elif opcao == 1:
             numero = input("Digite o número da conta: ")
             valor_add = float(input("Digite o valor a ser creditado: "))
-            Banco.creditar(numero, valor_add)
+            sisbanco.creditar(numero, valor_add)
 
-        if opcao == 2:
+        elif opcao == 2:
             numero = input("Digite o número da conta: ")
             valor_sub = float(input("Digite o valor a ser debitado: "))
-            Banco.debitar(numero, valor_sub)
+            sisbanco.debitar(numero, valor_sub)
         
-        if opcao == 3:
+        elif opcao == 3:
             num_origem = input("Digite o número da conta de origem: ")
             num_destino = input("Digite o número da conta de destino: ")
             valor_trans = float(input("Digite o valor a ser transferido: "))
-            Banco.transferir(num_origem, num_destino, valor_trans)
+            sisbanco.transferir(num_origem, num_destino, valor_trans)
         
-        if opcao == 4:
+        elif opcao == 4:
             numero = input("Digite o número da conta: ")
-            Banco.saldo(numero)
+            sisbanco.saldo(numero)
 
-        if opcao == 5:
+        elif opcao == 5:
             numero = input("Digite o número da conta: ")
-            Banco.render_juros(numero)
+            sisbanco.render_juros(numero)
         
-        if opcao == 6:
+        elif opcao == 6:
             numero = input("Digite o número da conta: ")
-            Banco.render_bonus(numero)
+            sisbanco.render_bonus(numero)
 
-        if opcao == 7:
-            pass
-        #Falta implementar essa parte 
+        elif opcao == 7:
+            try:
+                nova_taxa = float(input('Determine a nova taxa:'))
+                sisbanco.set_taxa(nova_taxa)
+
+            except ValueError:
+                print("Erro: A taxa informada não é válida. Por favor, insira um número.")
 
         elif opcao == 8:
             print("SisBanco::Bye!")
