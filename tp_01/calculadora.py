86db6c7ef3991a7e1f863aafb45cb041cac143ed
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-12-16 20:58:33 +0000

    criando memória na calculadora

--- tp_01/calculadora.py
+++ tp_01/calculadora.py
@@ -1,17 +1,38 @@
 #Temos um módulo, arquivo com definições, que executa as operações elementares através de funções. 
 #Decomposição procedural
+acumulador = 0.0
 
 def soma(operando_a, operando_b):
-    return operando_a + operando_b
+    global acumulador 
+    if operando_b is None:
+        acumulador += operando_a
+    else: 
+        acumulador = operando_a + operando_b
+    return acumulador
 
 def subtracao(operando_a, operando_b):
-    return operando_a - operando_b
+    global acumulador 
+    if operando_b is None:
+        acumulador -= operando_a
+    else:
+        acumulador = operando_a - operando_b
+    return acumulador
 
 def multiplicacao(operando_a, operando_b):
-    return operando_a * operando_b
+    global acumulador
+    if operando_b is None:
+        acumulador *= operando_a
+    else:
+        acumulador = operando_a * operando_b
+    return acumulador
 
 def divisao(operando_a, operando_b):
-    try:
-        return operando_a/operando_b
+    global acumulador 
+    try: 
+        if operando_b is None:
+            acumulador = acumulador/operando_b
+        else:
+            acumulador = operando_a/operando_b
     except ZeroDivisionError:
-        print("Não é possível realizar divisão por zero")
+            print("Não é possível realizar divisão por zero")
+    return acumulador
