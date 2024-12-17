c43fb81742e528fe85686ad2bc73a5f902289204
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2024-12-17 00:49:08 +0000

    calculadora POO finalizada

--- tp_02/calculadora_oo.py
+++ tp_02/calculadora_oo.py
@@ -5,17 +5,41 @@
     # - funções associadas a uma classe ou a um objeto.
     # - Eles definem os comportamentos que os objetos dessa classe podem realizar.
 class calculadora:
-    def soma(self, operando_a, operando_b):
-        return operando_a + operando_b
+    def __init__(self):
+        self.__acumulador = 0.0
+
+    def soma(self, operando_a, operando_b = None):
+        if operando_b is None:
+           self.__acumulador += operando_a
+        else: 
+           self.__acumulador = operando_a + operando_b
+        
+        return self.__acumulador
         
     def subtracao(self, operando_a, operando_b = None):
-        return operando_a - operando_b
+        if operando_b is None:
+           self.__acumulador *= operando_a
+        else: 
+           self.__acumulador = operando_a - operando_b
+        
+        return self.__acumulador
      
     def multiplicacao(self, operando_a, operando_b = None):
-        return operando_a * operando_b
+        if operando_b is None:
+           self.__acumulador /= operando_a
+        else: 
+           self.__acumulador = operando_a * operando_b
+        
+        return self.__acumulador
 
     def divisao(self, operando_a, operando_b = None): 
-        try: 
-            return operando_a/operando_b
-        except ZeroDivisionError:
-                print("Não é possível realizar divisão por zero")
+        try:
+            if operando_b is None:
+                self.__acumulador += operando_a
+            else: 
+                self.__acumulador = operando_a / operando_b
+        
+            return self.__acumulador
+        except:
+            print('Divisão por zero não existe.')
+
--- tp_02/testa_calculadora_oo.py
+++ tp_02/testa_calculadora_oo.py
@@ -5,8 +5,8 @@ def teste():
     #-É o processo de criar um objeto a partir de uma classe.
     #-Isso é feito chamando a classe como se fosse uma função, usando parênteses ().
     calc = calculadora()
-    res_soma = calc.soma(2,3)
-    res_sub = calc.subtracao(24, 20)
+    res_soma = calc.soma(2)
+    res_sub = calc.subtracao(5)
     res_mult = calc.multiplicacao(24, 20)
     res_div = calc.divisao(24, 20)
 
