636189a2331e56ec5fc59acc2296244438b8d811
Tamires Paulino <79025203+tamires123-hub@users.noreply.github.com>
2025-02-02 14:56:02 +0000

    Levantando execeções

--- tp_06/sisbanco.py
+++ tp_06/sisbanco.py
@@ -7,16 +7,9 @@ class ContaAbstrata(ABC):
       self._saldo = 0.0
 
    def creditar(self, valor:float) -> None:
-         if valor == None or valor < 0:
+         if valor is None or valor < 0:
             raise VIException(self.__numero, valor)
          self._saldo += valor
-
-         try: 
-            conta = Conta("143-A")
-            conta.creditar(-10)
-         except VIException as e:
-            e.print_mensagem_erro()
-
 
    @abstractmethod
    def debitar(self, valor:float) -> None:
@@ -34,30 +27,18 @@ class Conta(ContaAbstrata):
       super().__init__(numero)
 
    def debitar(self, valor:float) -> None:
-      if valor == None or valor < 0:
-         raise VIException(self.__numero, valor)
+      if valor is None or valor < 0:
+         raise VIException(self.get_numero(), valor)
       self._saldo -= valor
-
-      try: 
-         conta = Conta("630-F")
-         conta.debitar(5)
-      except VIException as e:
-         e.print_mensagem_erro()
 
 class ContaPoupanca(Conta):
    def __init__(self, numero:str):
       super().__init__(numero)
 
    def render_juros(self, taxa:float) -> None:
-      if taxa == None or taxa <= 0:
-         raise TJIException(self.__numero, taxa)
+      if taxa is None or taxa <= 0:
+         raise TJIException(self.get_numero(), taxa)
       self.creditar(self.get_saldo() * taxa)
-
-      try:
-         conta = Conta("745-P")
-         conta.render_juros(None)
-      except TJIException as e:
-         e.print_mensagem_erro()
 
 
 class ContaEspecial(Conta):
@@ -80,15 +61,8 @@ class ContaImposto(ContaAbstrata):
 
    def debitar(self, valor:float) -> None:
       if (valor + (valor * self.__taxa)) > self._saldo:
-         raise SIException(self.__numero, self._saldo)
+         raise SIException(self.get_numero(), self.get_saldo())
       self._saldo -= (valor + (valor * self.__taxa))
-
-      try:
-         conta = Conta("005-G")
-         conta.debitar(50)
-
-      except SIException as e:
-         e.print_mensagem_erro()
 
    def get_taxa(self) -> float:
       return self.__taxa
@@ -104,11 +78,17 @@ class Banco:
       self.__taxa_imposto = taxa_imposto
 
    def cadastrar(self, conta: ContaAbstrata) -> None:
-      if isinstance(conta, ContaImposto):
-         conta.set_taxa(self.__taxa_imposto)
-
-      self.__contas.append(conta)
+      if conta is None or not isinstance(conta, ContaAbstrata):
+         raise VIException(conta.get_numero(), 0.0)
       
+      if self.procurar(conta.get_numero()) is not None:
+         raise CEException(conta.get_numero())
+      
+      if isinstance(conta, ContaImposto):
+            conta.set_taxa(self.__taxa_imposto)
+      self.__contas.append(conta)
+
+
    def procurar(self, numero:str) -> ContaAbstrata:
       for conta in self.__contas:
          if conta.get_numero() == numero:
@@ -117,27 +97,31 @@ class Banco:
 
    def creditar(self, numero:str, valor:float) -> None:
       conta = self.procurar(numero)
-      if conta is not None:
-         conta.creditar(valor)
+      if conta is None:
+         raise CIException(numero)
+      conta.creditar(valor)
 
    def debitar(self, numero:str, valor:float) -> None:
       conta = self.procurar(numero)
-      if conta is not None:
-         conta.debitar(valor)
+      if conta is None:
+         raise CIException(numero)
+      conta.debitar(valor)
 
    def saldo(self, numero:str) -> float:
       conta = self.procurar(numero)
-      if conta is not None:
-         return conta.get_saldo()
-      return None
+      if conta is None:
+         raise CIException(numero)
+      return conta.get_saldo()
 
    def transferir(self, origem:str, destino:str, valor:float) -> None:
       conta_origem = self.procurar(origem)
-      if conta_origem is not None:
-         conta_destino = self.procurar(destino)
-         if conta_destino is not None:
-            conta_origem.debitar(valor)
-            conta_destino.creditar(valor)
+      conta_destino = self.procurar(destino)
+      if conta_origem is None: 
+         raise CIException(origem)
+      if conta_destino is None:
+         raise CIException(destino)
+      conta_origem.debitar(valor)
+      conta_destino.creditar(valor)
 
    def get_taxa_poupanca(self) -> float:
       return self.__taxa_poupanca
@@ -156,10 +140,12 @@ class Banco:
 
    def render_juros(self, numero:str) -> None:
       conta = self.procurar(numero)
-      if conta is not None and isinstance(conta, ContaPoupanca):
-         conta.render_juros(self.__taxa_poupanca)
+      if conta is None or not isinstance(conta, ContaPoupanca):
+         raise CIException(numero)
+      conta.render_juros(self.__taxa_poupanca)      
 
    def render_bonus(self, numero:str) -> None:
       conta = self.procurar(numero)
-      if conta is not None and isinstance(conta, ContaEspecial):
-         conta.render_bonus()
+      if conta is None or not isinstance(conta, ContaEspecial):
+         raise CIException(numero)
+      conta.render_bonus()
